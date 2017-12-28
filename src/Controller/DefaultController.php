95219aae3a03ea7eb4b88edee1b46f086cf4c6d5
tarlepp <tarmo.leppanen@protacon.com>
2017-12-28 21:15:06 +0200

    Initial commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wunder
- * Date: 28.12.2017
- * Time: 21:08
- */
+declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+/**
+ * Class DefaultController
+ *
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController
 {
-
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('default.html.twig');
+    }
 }
